ac1e14085f3f1ddd25ee0a8ed6520faaf6db8327
felbecker <beckerfelix94@gmail.com>
2025-04-24 07:58:37 +0000

    fixed get_homfam script, changed default threads of one rule

--- data/homfam/get_homfam.sh
+++ data/homfam/get_homfam.sh
@@ -5,10 +5,24 @@ mkdir aligned
 mkdir unaligned
 # move all files that match *_ref.vie into aligned and strip the _ref 
 # move the rest with the pattern *_test-only.vie into unaligned and strip "_test-only"
+# also append the reference sequences to the test sequences and remove gaps
 for file in *_ref.vie; do
     mv "$file" "aligned/$(basename "$file" _ref.vie).vie"
 done
 for file in *_test-only.vie; do
-    mv "$file" "unaligned/$(basename "$file" _test-only.vie).vie"
+    # add references to the test sequences
+    target_file=$(basename "$file" _test-only.vie).vie
+    mv "$file" "unaligned/$target_file"
+    cat "unaligned/$target_file" "aligned/$target_file" > "unaligned/$target_file.tmp"
+    # remove gaps and keep file permissions
+    sed -i '/^>/!s/[-.]//g' "unaligned/$target_file.tmp" #remove gaps
+    # Save original permissions
+    PERMS=$(stat -c %a unaligned/$target_file)
+    # Make the file writable (temporarily)
+    chmod u+w unaligned/$target_file
+    # Replace it
+    mv -f "unaligned/$target_file.tmp" "unaligned/$target_file"
+    # Restore original permissions
+    chmod $PERMS "unaligned/$target_file"
 done
 rm -r homfam-20110613-25.tar.gz
