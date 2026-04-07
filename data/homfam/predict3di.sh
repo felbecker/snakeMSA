67be5cb2c8fa8892f60831d01fb35cfa12c4edee
felbecker <beckerfelix94@gmail.com>
2026-04-07 13:49:07 +0200

    added predict3di.sh

--- data/homfam/predict3di.sh
+++ data/homfam/predict3di.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # compute 3di predictions for the homfam dataset
-mkdir -p 3di
+mkdir -p predicted_3di
 for file in unaligned/*.vie; do
-    output="3di/$(basename "${file%.vie}.fasta")"
+    output="predicted_3di/$(basename "${file%.vie}.fasta")"
     if [ -f "$output" ]; then
         echo "Skipping $file (output already exists)"
         continue
     fi
     echo "Processing $file"
-    python ../../../3DUtil/predict3di.py \
+    python 3DUtil/predict3di.py \
     --fasta "$file" \
     --output "$output" \
-    --prostt5-model "../../../3DUtil/prostT5"
+    --prostt5-model "3DUtil/prostT5"
 done
