6c88e305e5d9d200d7a84eefcf6150de193a2626
felbecker <beckerfelix94@gmail.com>
2026-05-05 10:37:30 +0200

    updated ext homfam download script

--- data/ext_homfam/get_ext_homfam.sh
+++ data/ext_homfam/get_ext_homfam.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
-wget https://zenodo.org/record/6524237/files/extHomFam-v2.zip
-python3 -m zipfile -e extHomFam-v2.zip .
-X=("small" "medium" "large" "xlarge" "huge")
-for folder in "${X[@]}"
-do
-    mkdir ext_homfam_$folder
-    mkdir ext_homfam_$folder/aligned
-    mkdir ext_homfam_$folder/unaligned
-    mv extHomFam-v2/$folder/* ext_homfam_$folder/unaligned/
-    for file in ext_homfam_$folder/unaligned/*
-    do
-        filename=$(basename "$file")
-        mv "extHomFam-v2/ref/$filename" "ext_homfam_$folder/aligned"
-    done
+
+if [ -d "unaligned" ]; then
+    echo "unaligned/ already exists, skipping download and extraction."
+else
+    wget https://zenodo.org/records/15883613/files/extHomFam-v37.0.tar.gz
+    tar -xzf extHomFam-v37.0.tar.gz
+    mv extHomFam-v37.0/families unaligned/
+    mv extHomFam-v37.0/references aligned/
+fi
+
+for file in unaligned/*; do
+    python uniquify.py "$file"
 done
-rm -r extHomFam-v2
-rm extHomFam-v2.zip
-python3 uniquify.py ext_homfam_large/unaligned/DNA_pol3_beta
-python3 uniquify.py ext_homfam_xlarge/unaligned/ldh
-python3 uniquify.py ext_homfam_huge/unaligned/gtp
-python3 uniquify.py ext_homfam_huge/unaligned/thiored
-python3 uniquify.py ext_homfam_huge/unaligned/Epimerase
-python3 uniquify.py ext_homfam_huge/unaligned/apbact
+for file in aligned/*; do
+    python uniquify.py "$file"
+done
 
 # Final postprocessing: remove write permission for file owner
-for folder in "${X[@]}"
-do
-    chmod u-w ext_homfam_$folder/unaligned/*
-    chmod u-w ext_homfam_$folder/aligned/*
-done
+chmod u-w unaligned/*
+chmod u-w aligned/*
